3d2e06f08a38119f6fcbaf7d4e34c58b6f4f0033
alexneufeld <alex.d.neufeld@gmail.com>
2023-09-24 01:02:22 -0600

    fix typos

--- freecad/robotpy/app/Robot6Axis.py
+++ freecad/robotpy/app/Robot6Axis.py
@@ -4,12 +4,11 @@ import csv
 import math
 from dataclass import dataclass
 from . import RobotAlgos
-import csv
 import os
 
 
 @dataclass
-class AxisDefinition
+class AxisDefinition:
     a: float = 0.0  # a of the Denavit-Hartenberg parameters (mm)
     alpha: float = 0.0  # alpha of the Denavit-Hartenberg parameters (°)
     d: float = 0.0  # d of the Denavit-Hartenberg parameters (mm)
@@ -21,16 +20,17 @@ class AxisDefinition
 
 
 KukaIR500 = [
-    # (a    ,alpha ,d    ,theta ,rotDir ,maxAngle ,minAngle ,AxisVelocity 
-    AxisDefinition(500  ,-90   ,1045 ,0     , -1    ,+185     ,-185     ,156         ), # Axis 1
-    AxisDefinition(1300 ,0     ,0    ,0     , 1     ,+35      ,-155     ,156         ), # Axis 2
-    AxisDefinition(55   ,+90   ,0    ,-90   , 1     ,+154     ,-130     ,156         ), # Axis 3
-    AxisDefinition(0    ,-90   ,-1025,0     , 1     ,+350     ,-350     ,330         ), # Axis 4
-    AxisDefinition(0    ,+90   ,0    ,0     , 1     ,+130     ,-130     ,330         ), # Axis 5
-    AxisDefinition(0    ,+180  ,-300 ,0     , 1     ,+350     ,-350     ,615         )  # Axis 6
+    # (a    ,alpha ,d    ,theta ,rotDir ,maxAngle ,minAngle ,AxisVelocity
+    AxisDefinition(500, -90, 1045, 0, -1, +185, -185, 156),  # Axis 1
+    AxisDefinition(1300, 0, 0, 0, 1, +35, -155, 156),  # Axis 2
+    AxisDefinition(55, +90, 0, -90, 1, +154, -130, 156),  # Axis 3
+    AxisDefinition(0, -90, -1025, 0, 1, +350, -350, 330),  # Axis 4
+    AxisDefinition(0, +90, 0, 0, 1, +130, -130, 330),  # Axis 5
+    AxisDefinition(0, +180, -300, 0, 1, +350, -350, 615),  # Axis 6
 ]
 
-class Robot6Axis
+
+class Robot6Axis:
     def __init__(self) -> None:
         self._Kinematic = PyKDL.Chain()
         self._Actual = PyKDL.JntArray(6)
@@ -40,24 +40,30 @@ class Robot6Axis
         self._Velocity = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self._RotDir = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self.setKinematic(KukaIR500)
-    
-    def setKinematic(self, KinDef: list[AxisDefinition) -> None:
+
+    def setKinematic(self, KinDef: list[AxisDefinition]) -> None:
         temp = PyKDL.Chain()
         for i in range(6):
             temp.addSegment(
                 PyKDL.Segment(
                     PyKDL.Joint(PyKDL.Joint.RotZ),
-                    PyKDL.Frame.DH(KinDef[i].a, KinDef[i].alpha * (math.pi/180) ,KinDef[i].d ,KinDef[i].theta * (math.pi/180)))
+                    PyKDL.Frame.DH(
+                        KinDef[i].a,
+                        KinDef[i].alpha * (math.pi / 180),
+                        KinDef[i].d,
+                        KinDef[i].theta * (math.pi / 180),
+                    ),
+                )
             )
-            self._RotDir  [i] = KinDef[i].rotDir
-            self._Max(i) = KinDef[i].maxAngle * (math.pi/180)
-            self._Min(i) = KinDef[i].minAngle * (math.pi/180)
+            self._RotDir[i] = KinDef[i].rotDir
+            self._Max[i] = KinDef[i].maxAngle * (math.pi / 180)
+            self._Min[i] = KinDef[i].minAngle * (math.pi / 180)
             self._Velocity[i] = KinDef[i].velocity
         self._Kinematic = temp
         self.calcTcp()
-    
-    def readKinematic(self FileName: os.PathLike) -> None:
-        with open(FileName, 'r') as f:
+
+    def readKinematic(self, FileName: os.PathLike) -> None:
+        with open(FileName, "r") as f:
             reader = csv.reader(f, dialect="excel")
             # skip header
             next(reader)
@@ -65,38 +71,44 @@ class Robot6Axis
             self.setKinematic(temp)
 
     def setTo(self, To: FreeCAD.Placement) -> bool:
-        fksolver1 = PyKDL.ChainFkSolverPos_recursive(self._Kinematic)  # Forward position solver
-        iksolver1v = PyKDL.ChainIkSolverVel_pinv(self._Kinematic)  # Inverse velocity solver
-        iksolver1 = ChainIkSolverPos_NR_JL(self._Kinematic,self._Min,self._Max,fksolver1,iksolver1v,100,1e-6)  # Maximum 100 iterations, stop at accuracy 1e-6
+        fksolver1 = PyKDL.ChainFkSolverPos_recursive(
+            self._Kinematic
+        )  # Forward position solver
+        iksolver1v = PyKDL.ChainIkSolverVel_pinv(
+            self._Kinematic
+        )  # Inverse velocity solver
+        iksolver1 = PyKDL.ChainIkSolverPos_NR_JL(
+            self._Kinematic, self._Min, self._Max, fksolver1, iksolver1v, 100, 1e-6
+        )  # Maximum 100 iterations, stop at accuracy 1e-6
         # Creation of jntarrays:
         result = PyKDL.JntArray(self._Kinematic.getNrOfJoints())
         # Set destination frame
         F_dest = RobotAlgos.toFrame(To)
         # solve
-        if (iksolver1.CartToJnt(self._Actual,F_dest,result) < 0):
-            return false
+        if iksolver1.CartToJnt(self._Actual, F_dest, result) < 0:
+            return False
         else:
-            self._Actual = result;
-            self._Tcp = F_dest;
-            return true
-    
-    def setAxis(self, Axis: int, Value: float) ->bool:
+            self._Actual = result
+            self._Tcp = F_dest
+            return True
+
+    def setAxis(self, Axis: int, Value: float) -> bool:
         self._Actual[Axis] = self._RotDir
-    
+
     def getAxis(self, Axis: int) -> float:
         return self._RotDir[Axis] * (self._Actual[Axis] / (math.pi / 180.0))
-    
+
     def getMaxAngle(self, Axis: int) -> float:
         return self._Max[Axis] * 180.0 / math.pi
-    
+
     def getMinAngle(self, Axis: float) -> float:
         return self._Min[Axis] * 180.0 / math.pi
-    
+
     def calcTcp(self) -> bool:
         fksolver = PyKDL.ChainFkSolverPos_recursive(self._Kinematic)
-        cartpos = PyKDL.Frame()
+        self.cartpos = PyKDL.Frame()
         kinematics_status = fksolver.JntToCart(self._Actual, self.cartpos)
-        return (kinematics_status >= 0)
-    
+        return kinematics_status >= 0
+
     def getTcp(self) -> FreeCAD.Placement:
         return RobotAlgos.toPlacement(self._Tcp)
