152a67ed1a1831b0c06db3af83ad9a991698d7be
alexneufeld <alex.d.neufeld@gmail.com>
2023-09-29 18:48:45 -0600

    almost working robot object

--- freecad/robotpy/gui/ViewProviderRobotObject.py
+++ freecad/robotpy/gui/ViewProviderRobotObject.py
@@ -1,4 +1,5 @@
 from freecad.robotpy import ICONPATH
+
 # from freecad.robotpy import RobotObject
 import FreeCAD
 import FreeCADGui
@@ -7,72 +8,81 @@ from pivy import coin
 from enum import Enum
 from math import pi
 
+
 class highlightModes(Enum):
-    """reimplements the c++ equivalent for this, because I can't figure out a way to 
+    """reimplements the c++ equivalent for this, because I can't figure out a way to
     access the original from python.
-    ref: https://github.com/FreeCAD/FreeCAD/blob/5a81fcd7a5019a5cbf35f9d410e629aae6a486a7/src/Gui/SoFCSelection.h"""
+    ref: https://github.com/FreeCAD/FreeCAD/blob/5a81fcd7a5019a5cbf35f9d410e629aae6a486a7/src/Gui/SoFCSelection.h
+    """
+
     AUTO = 0
     ON = 1
     OFF = 2
+
 
 class ViewProviderRobotObject:
     def __init__(self, vobj) -> None:
         vobj.addProperty("App::PropertyBool", "Manipulator").Manipulator = False
         self.selnode = coin.SoType.fromName("SoFCSelection").createInstance()
-        vobj.Proxy = self
         # ref: https://github.com/FreeCAD/FreeCAD/blob/d95b26dfb5dd23eaee004b4af3cf5cf4766e7f9c/src/Mod/Draft/draftguitools/gui_trackers.py#L832
         self.pcRobotRoot = coin.SoType.fromName("SoFCSelection").createInstance()
         self.pcRobotRoot.highlightMode = highlightModes.OFF.value
 
         self.pcSimpleRoot = coin.SoType.fromName("SoFCSelection").createInstance()
         self.pcSimpleRoot.highlightMode = highlightModes.OFF.value
-        
-        self.pcOffRoot = coin.soGroup()
-        
+
+        self.pcOffRoot = coin.SoGroup()
+
         # set nodes for the manipulator outfit
-        self.pcTcpRoot = coin.soGroup()
-        self.Axis1Node = self.Axis2Node = self.Axis3Node = self.Axis4Node = self.Axis5Node = self.Axis6node = None
+        self.pcTcpRoot = coin.SoGroup()
+        self.Axis1Node = None
+        self.Axis2Node = None
+        self.Axis3Node = None
+        self.Axis4Node = None
+        self.Axis5Node = None
+        self.Axis6node = None
         self.pcDragger = None
+        vobj.Proxy = self
 
     def setDragger(self) -> None:
-        assert (self.pcDragger is None)
+        assert self.pcDragger is None
         self.pcDragger = coin.SoJackDragger()
-        self.pcDragger.addMotionCallback(self.sDraggerMotionCallback)
+        self.pcDragger.addMotionCallback(self.soDraggerMotionCallback)
         self.pcTcpRoot.addChild(self.pcDragger)
         # set the actual TCP position
         # TODO: missing PyKDL bindings for this to work
         # robObj = static_cast<Robot::RobotObject*>(pcObject);
         # robObj = pcObject
-        loc = robObj.Tcp.getValue()
+        loc = self.obj.Tcp.getValue()
         M = coin.SbMatrix()
         M.setTransform(
-            coin.SbVec3f(loc.getPosition().x,loc.getPosition().y,loc.getPosition().z),
+            coin.SbVec3f(loc.getPosition().x, loc.getPosition().y, loc.getPosition().z),
             coin.SbRotation(*loc.getRotation()),
-            coin.SbVec3f(150,150,150)
+            coin.SbVec3f(150, 150, 150),
         )
         self.pcDragger.setMotionMatrix(M)
-    
+
     def resetDragger(self) -> None:
-        assert (self.pcDragger is not None)
+        assert self.pcDragger is not None
         FreeCADGui.coinRemoveAllChildren(self.pcTcpRoot)
         self.pcDragger = None
-        
+
     def attach(self, vobj) -> None:
         vobj.addDisplayMode(self.pcRobotRoot, "VRML")
         self.pcRobotRoot.objectName = vobj.Object.Name
         self.pcRobotRoot.documentName = vobj.obj.Document.Name
         self.pcRobotRoot.subElementName = "Main"
         self.pcRobotRoot.addChild(self.pcTcpRoot)
-        
+
         vobj.addDisplayMode(self.pcSimplRoot, "Simple")
         self.pcSimpleRoot.objectName = vobj.Object.Name
         self.pcSimpleRoot.documentName = vobj.obj.Document.Name
         self.pcSimpleRoot.subElementName = "Main"
         self.pcSimpleRoot.addChild(self.pcTcpRoot)
-        
+
         vobj.addDisplayMode(self.pcOffRoot, "Off")
         self.pcOffRoot.addChild(self.pcTcpRoot)
-    
+
     def getDisplayModes(self, vobj) -> list[str]:
         return ["VRML", "Simple", "Off"]
 
@@ -81,7 +91,7 @@ class ViewProviderRobotObject:
 
     def setDisplayMode(self, mode):
         return mode
-    
+
     def onChanged(self, vobj, prop) -> None:
         if prop == "Manipulator":
             if vobj.Manipulator:
@@ -92,7 +102,7 @@ class ViewProviderRobotObject:
                     self.resetDragger()
         else:
             pass
-    
+
     def updateData(self, obj, prop) -> None:
         if prop == "RobotVrmlFile":
             FreeCADGui.coinRemoveAllChildren(self.pcRobotRoot)
@@ -102,18 +112,23 @@ class ViewProviderRobotObject:
                 raise RuntimeError(f"Cannot open file {obj.RobotVrmlFile}")
             node = coin.SoDB.readAll(mySceneInput)
             if node:
-                pcRobotRoot.addChild(node)
-            pcRobotRoot.addChild(pcTcpRoot)
+                self.pcRobotRoot.addChild(node)
+            self.pcRobotRoot.addChild(self.pcTcpRoot)
 
             # search for the connection points
-            self.Axis1Node = self.Axis2Node = self.Axis3Node = self.Axis4Node = self.Axis5Node = self.Axis6node = None
+            self.Axis1Node = None
+            self.Axis2Node = None
+            self.Axis3Node = None
+            self.Axis4Node = None
+            self.Axis5Node = None
+            self.Axis6node = None
 
             searchAction = coin.SoSearchAction()
             path = coin.SoPath()
 
             # Axis 1
             searchAction.setName("FREECAD_AXIS1")
-            searchAction.setInterest(SoSearchAction.FIRST)
+            searchAction.setInterest(coin.SoSearchAction.FIRST)
             searchAction.setSearchingAll(False)
             searchAction.apply(self.pcRobotRoot)
             path = searchAction.getPath()
@@ -123,7 +138,7 @@ class ViewProviderRobotObject:
                     self.Axis1Node = node
             # Axis 2
             searchAction.setName("FREECAD_AXIS2")
-            searchAction.setInterest(SoSearchAction.FIRST)
+            searchAction.setInterest(coin.SoSearchAction.FIRST)
             searchAction.setSearchingAll(False)
             searchAction.apply(self.pcRobotRoot)
             path = searchAction.getPath()
@@ -133,7 +148,7 @@ class ViewProviderRobotObject:
                     self.Axis2Node = node
             # Axis 3
             searchAction.setName("FREECAD_AXIS3")
-            searchAction.setInterest(SoSearchAction.FIRST)
+            searchAction.setInterest(coin.SoSearchAction.FIRST)
             searchAction.setSearchingAll(False)
             searchAction.apply(self.pcRobotRoot)
             path = searchAction.getPath()
@@ -143,7 +158,7 @@ class ViewProviderRobotObject:
                     self.Axis3Node = node
             # Axis 4
             searchAction.setName("FREECAD_AXIS4")
-            searchAction.setInterest(SoSearchAction.FIRST)
+            searchAction.setInterest(coin.SoSearchAction.FIRST)
             searchAction.setSearchingAll(False)
             searchAction.apply(self.pcRobotRoot)
             path = searchAction.getPath()
@@ -153,7 +168,7 @@ class ViewProviderRobotObject:
                     self.Axis4Node = node
             # Axis 5
             searchAction.setName("FREECAD_AXIS5")
-            searchAction.setInterest(SoSearchAction.FIRST)
+            searchAction.setInterest(coin.SoSearchAction.FIRST)
             searchAction.setSearchingAll(False)
             searchAction.apply(self.pcRobotRoot)
             path = searchAction.getPath()
@@ -161,10 +176,10 @@ class ViewProviderRobotObject:
                 node = path.getTail()
                 if node and (node.getTypeId() == coin.SoVRMLTransform.getClassTypeId()):
                     self.Axis5Node = node
-                    
+
             # Axis 6
             searchAction.setName("FREECAD_AXIS6")
-            searchAction.setInterest(SoSearchAction.FIRST)
+            searchAction.setInterest(coin.SoSearchAction.FIRST)
             searchAction.setSearchingAll(False)
             searchAction.apply(self.pcRobotRoot)
             path = searchAction.getPath()
@@ -203,13 +218,17 @@ class ViewProviderRobotObject:
                     coin.SbVec3f(0.0, 1.0, 0.0),
                     obj.Axis6 * pi / 180,
                 )
-            
 
-
-
-
-
-    def setAxisTo(self, A1: float, A2: float, A3: float, A4: float, A5: float, A6: float, Tcp: FreeCAD.Placement) -> None:
+    def setAxisTo(
+        self,
+        A1: float,
+        A2: float,
+        A3: float,
+        A4: float,
+        A5: float,
+        A6: float,
+        Tcp: FreeCAD.Placement,
+    ) -> None:
         pass
 
     def getIcon(self):
