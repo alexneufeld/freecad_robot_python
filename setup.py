cdd5e3f8f4c47ea39236e6d891355b3c8fbd1487
alexneufeld <alex.d.neufeld@gmail.com>
2023-10-04 19:55:20 -0600

    run pre-commit on everything

--- setup.py
+++ setup.py
@@ -1,29 +1,17 @@
 from setuptools import setup
-import os
 
-# from freecad.workbench_starterkit.version import __version__
-# name: this is the name of the distribution.
-# Packages using the same name here cannot be installed together
-
-version_path = os.path.join(
-    os.path.abspath(os.path.dirname(__file__)),
-    "freecad",
-    "workbench_starterkit",
-    "version.py",
-)
-with open(version_path) as fp:
-    exec(fp.read())
+__version__ = "0.0.0"  # FIXME
 
 setup(
-    name="freecad.workbench_starterkit",
+    name="freecad.robot_python",
     version=str(__version__),
-    packages=["freecad", "freecad.workbench_starterkit"],
-    maintainer="looooo",
-    maintainer_email="sppedflyer@gmail.com",
-    url="https://github.com/FreeCAD/Workbench-Starterkit",
-    description="template for a freecad extensions, installable with pip",
+    packages=["freecad", "freecad.robot_python"],
+    maintainer="Alex Neufeld",
+    maintainer_email="alex.d.neufeld@gmail.com",
+    url="github.com/alexneufeld/freecad_robot_python/",
+    description="A FreeCAD addon for simulating multi-axis robots",
     install_requires=[
-        "numpy"
-    ],  # should be satisfied by FreeCAD's system dependencies already
+        "PyKDL",
+    ],
     include_package_data=True,
 )
